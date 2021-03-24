1288cb85f0b802147ce83b3b46ed42145c1928ae
bdawghoya28 <78703992+bdawghoya28@users.noreply.github.com>
2021-03-24 01:39:43 -0500

    Add files via upload

---
+++ Week 1.R
@@ -0,0 +1,15 @@
+# Week 1 - Introduction to Machine Learning ####
+
+# Set up and Data ####
+
+# Set Working Directory
+setwd("~/OPIM 602 - Machine Learning I")
+
+# Load libraries
+library(readr)
+library(tidyverse)
+library(mlr3)
+
+# Load the dataset 
+Titanic_Dataset <- read_csv("Data/Titanic Dataset.csv")
+
