155ae21aa47caab79d08aef9a4e46281dbe1ec3b
bdawghoya28 <78703992+bdawghoya28@users.noreply.github.com>
2021-03-24 01:39:06 -0500

    Add files via upload

---
+++ Project 1.R
@@ -0,0 +1,23 @@
+# Project 1 - Exploratory Data Analysis
+
+# Set up and Data ####
+
+# Set Working Directory
+setwd("~/OPIM 602 - Machine Learning I")
+
+# Load libraries
+library(readr)
+library(tidyverse)
+library(mlr3)
+
+# Load the dataset 
+Bike_Share <- read_csv("Data/Capital Bike Sharing data by hour.csv")
+
+# EDA: Time Series ####
+
+
+# EDA: Rider Segments ####
+
+
+# EDA: Weather Trends ####
+
